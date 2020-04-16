1bbd061bd200e3e0c6c10ea0d6548a11b47e99a6
Sunny Sehgal <Sunny.Sehgal@INFOVISIONLABS.COM>
2020-04-16 12:53:17 +0530

    Route Guard or Auth Gaurd Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+import { ActivateGuard } from './activate.guard';
+import { UserService } from './user.service';
+import { HomeComponent } from './home/home.component';
+import { Approutes } from './Routing'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot( Approutes )
   ],
-  providers: [],
+  providers: [ActivateGuard, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
